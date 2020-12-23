926146a7ef63e58838f5d7068c62d67796069f20
Daniil <dan.bardyuk@yandex.ru>
2020-12-23 21:50:12 +0300

    creat class Box

--- Main.java
+++ Main.java
@@ -3,3 +3,18 @@ class Main{
 
     }
 }
+
+class Box{
+    public double weidht;
+    public double height;
+    public double depth;
+
+    Box(double w, double h, double d){
+        weidht = w;
+        height = h;
+        depth = d;
+    }
+    public double volume(){
+        return weidht*depth*height;
+    }
+}
